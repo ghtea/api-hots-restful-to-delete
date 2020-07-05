a7f1baa4e06dcdc4074fbc25a6042b54b8231b14
ghtea <eiirwp@gmail.com>
2020-07-05 09:08:11 +0000

    book api testing ended

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,49 +1,95 @@
-module.exports = function(app, Book)
-{
+module.exports = function(app, Book) {
 
-app.get('/books', (req, res) => {
-  return res.send('GET HTTP method on user resource');
-});
-
- /*
-app.post('/books', (req, res) => {
-  return res.send('POST HTTP method on user resource');
-});
-*/
-
-app.post('/books', function(req, res){
+  // GET ALL BOOKS
+  app.get('/books', (req, res) => {
+    Book.find((err, books) => {
+      if (err) return res.status(500).send({
+        error: 'database failure'
+      });
+      res.json(books);
+    })
+  });
+  
+  
+  // GET SINGLE BOOK
+  app.get('/books/:idBook', (req, res) => {
+    Book.findOne({_id: req.params.idBook}, (err, book) => {
+      if(err) return res.status(500).json({error: err});
+      if(!book) return res.status(404).json({error: 'book not found'});
+      res.json(book);
+    })
+  });
+    
+  
+  
+  // GET BOOKS BY AUTHOR
+  app.get('/books/author/:author', (req, res) => {
+    Book.find({author: req.params.author}, {_id: 0, title: 1, published_date: 1},  (err, books) => {
+      if(err) return res.status(500).json({error: err});
+      if(books.length === 0) return res.status(404).json({error: 'book not found'});
+      res.json(books);
+    })
+  });
+    
+  
+  
+  // CREATE BOOK
+  app.post('/books', (req, res) => {
     let book = new Book();
     book.title = req.body.title;
     book.author = req.body.author;
     //book.published_date = new Date(req.body.published_date);
 
-    book.save(function(err){
-    	
-        if(err){
-            console.error(err);
-            res.json({result: 0});
-            return;
-        }
+    book.save((err) => {
 
-        res.json({result: 1});
+      if (err) {
+        console.error(err);
+        res.json({
+          result: 0
+        });
+        return;
+      }
+
+      res.json({
+        result: 1
+      });
 
     });
-});
+  });
 
 
+  // UPDATE THE BOOK
+  app.put('/books/:idBook', (req, res) => {
+    Book.findById(req.params.idBook, (err, book) => {
+      if(err) return res.status(500).json({ error: 'database failure' });
+      if(!book) return res.status(404).json({ error: 'book not found' });
 
- 
-app.put('/books/:idBook', (req, res) => {
-  return res.send(
-    `PUT HTTP method on user/${req.params.idBook} resource`,
-  );
-});
- 
-app.delete('/books/:idBook', (req, res) => {
-  return res.send(
-    `DELETE HTTP method on user/${req.params.idBook} resource`,
-  );
-});
+      if(req.body.title) book.title = req.body.title;
+      if(req.body.author) book.author = req.body.author;
 
-	
+      book.save((err) => {
+        if(err) res.status(500).json({error: 'failed to update'});
+        res.json({message: 'book updated'});
+      });
+  
+    });
+  
+  });
+
+  
+  // DELETE BOOK
+  app.delete('/books/:idBook', (req, res) => {
+    Book.remove({ _id: req.params.idBook }, (err, output) => {
+      if(err) return res.status(500).json({ error: "database failure" });
+
+      /* ( SINCE DELETE OPERATION IS IDEMPOTENT, NO NEED TO SPECIFY )
+      if(!output.result.n) return res.status(404).json({ error: "book not found" });
+      res.json({ message: "book deleted" });
+      */
+
+      res.status(204).end();
+    })
+  });
+  
+
 }
